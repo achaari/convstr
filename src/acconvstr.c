c142098b852f8f8c9af5a3f80310150ce628f766
achaari <achaari@users.noreply.github.com>
2019-11-22 13:31:24 +0100

    Update acconvstr.c

--- src/acconvstr.c
+++ src/acconvstr.c
@@ -5,134 +5,86 @@
 
 typedef unsigned char acbyte;
 
-static void accmpr_getbytes(const acbyte *valarrp, int mlen, int *idvxp, int *lfp, int *rgp)
+static void actrns_encode_bytes(const acbyte *valarrp, int mlen, int *idvxp, int *valxp, int *idvlxp)
 {
-    if (*rgp == -1) {
-	int xval = (int) valarrp[mlen - 1 - (*idvxp)++];
-	*lfp = (xval & 120) >> 4;
-	*rgp = xval & 15;
-    }
-    else {
-	*lfp = *rgp;
-	*rgp = -1;
-    }
-}
+    unsigned int valx = 0, pwd = 0;
 
-static void accmpr_decode_idx(const acbyte *valarrp, int mlen,  int *idvxp, int *idrxp, int idrx, int *rgp)
-{
-    int lf = 0, op = 1, pr = 0, idx = 0, pwr = 0;
-    
-    while (op && (*idvxp <= mlen)) {
-	accmpr_getbytes(valarrp, mlen, idvxp, &lf, rgp);
-
-	op = lf & 1;
-	pr = lf >> 1;
-
-	if (pr) {
-	    idx += (pwr ? (1 << (pwr * 3)) : 1) * pr;
-	}
-
-	if ((++pwr > 3) && op) {
-	    idx += 1000;
-	    break;
+    for (int idx = 0; idx < 4; idx++) {
+	if ((*idvxp) < mlen) {
+	    valx += (idx ? (1 << (idx * 8)) : 1) * valarrp[(*idvxp)++];
 	}
     }
 
-    idrxp[idrx] = idx + idrx % 8;
-}
-
-static void accmpr_decode_idxarr(const acbyte *valarrp, int len, int *maxlvp, int **idxarp)
-{
-    *idxarp = calloc(2 * len + 1, sizeof(int));
-    *maxlvp = 0;
-    
-
-    if (idxarp == NULL) {
-	return;
-    }
-
-    int itr = 0, rg = -1;
-    while (itr <= len) {
-	accmpr_decode_idx(valarrp, len, &itr, *idxarp, (*maxlvp)++, &rg);
-    }
-}
-
-static const char *vrxtab = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-static char accmpr_get_randc()
-{
-    srand(time(0));
-
-    return vrxtab[rand() % 62];
-}
-
-static void accmpr_decode_strtab(const char *str, int *maxlvp, int **idxarp)
-{
-    char *codes;
-
-    codes = calloc(strlen(str) + 30, sizeof(char));
-
-    char crnd = accmpr_get_randc();
-
-    sprintf(codes, "%c:%s:%c%c%c:%d:%c", crnd, str, str[strlen(str)-1], crnd, str[0], strlen(str), crnd);
-
-    accmpr_decode_idxarr((const acbyte*)codes, strlen(codes), maxlvp, idxarp);
-
-    free(codes);
-    codes = NULL;
-}
-
-#define posex  33 
-#define posda  (posex + 1) 
-#define posvx  (posda + 10)	
-#define lista  200
-
-void accmpr_decode_str(const char *charx, char *txt)
-{
-    int lnx = 0;
-    int *lnarr = NULL;
-    
-    accmpr_decode_strtab(charx, &lnx, &lnarr);
-
-    int idx = 0, lsd, lvd;
-    char buff[10];
-    *txt = '\0';
-
-    while (idx <= lnx) {
-	if ((char)lnarr[idx] == '\0') {
-	    sprintf(buff, "%c", (acbyte)posex);
+    for (int idx = 0; idx < 5; idx++) {
+	if (idx == 0) {
+	    pwd = valx & 63;
+	}
+	else if (idx == 4) {
+	    pwd = valx >> 24;
 	}
 	else {
-	    lsd = lnarr[idx] / lista;
-	    lvd = lnarr[idx] % lista + posvx;
-
-	    if (lsd) {
-		sprintf(buff, "%c%c", (acbyte)(lsd + posvx), (acbyte)lvd);
-	    }
-	    else {
-		sprintf(buff, "%c", (acbyte)lvd);
-	    }
+	    pwd = (valx & (63 << (idx * 6))) >> (idx * 6);
 	}
-
-	strcat(txt, buff);
-
-	idx++;
+	valxp[(*idvlxp)++] = pwd;
     }
 
-    free(lnarr);
-    lnarr = NULL;
 }
 
-int main() 
+static void actrns_decode_bytes(const int *valxp, int mlen, int *idvxp, acbyte *valarrp, int *idarrxp)
+{
+    unsigned int valx = 0, pwd = 0;
+
+    for (int idx = 0; idx < 5; idx++) {
+	valx += (idx ? (1 << (idx * 6)) : 1) * valxp[(*idvxp)++];
+    }
+
+    for (int idx = 0; idx < 4; idx++) {
+	pwd = (valx & (255 << (idx * 8)));
+	if (pwd) {
+	    if (idx) {
+		pwd = pwd >> (idx * 8);
+	    }
+	    valarrp[(*idarrxp)++] = (acbyte)pwd;
+	}
+    }
+
+}
+
+static void actrns_encode_strtab(const char *valstrp, int **arrvalxp, int *idvalp)
+{
+    int stlen = strlen(valstrp);
+    
+    *arrvalxp = calloc(2 * stlen, sizeof(int));
+
+    int idx = 0;
+
+    while (idx < stlen) {
+	actrns_encode_bytes((const acbyte *)valstrp, stlen, &idx, *arrvalxp, idvalp);
+    }
+}
+
+static void actrns_decode_strtab(int *arrvalxp, int tablen, const char *valstrp, int *idstrp)
+{
+    int idx = 0;
+    while (idx < tablen) {
+	actrns_decode_bytes(arrvalxp, tablen, &idx, valstrp, idstrp);
+    }
+}
+
+static void actrns_enocde_string(const char *valstrp, char *outstrp)
+{
+    int *arrvalxp = NULL;
+    int idvalx = 0;
+    actrns_encode_strtab(valstrp, &arrvalxp, &idvalx);
+
+    int idstdx = 0;
+    actrns_decode_strtab(arrvalxp, idvalx, outstrp, &idstdx);
+}
+
+int main()
 {
     const char *charx = "this is my first string tests !!!";
     char txt[500];
 
-    accmpr_decode_str(charx, txt);
-    
-    int aa = strlen(txt) - strlen(charx);
-
-    printf(txt);
-
-    return 0;
+    actrns_enocde_string(charx, txt);
 }
